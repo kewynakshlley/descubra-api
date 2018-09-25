276d370e88dafadc97e3d1dee56d0ac6c984e3ec
Kewyn Akshlley <kewynakshlley@gmail.com>
2018-09-25 20:43:45 -0300

    cross origin

--- src/main/java/co/descubra/descubraapi/controllers/EventController.java
+++ src/main/java/co/descubra/descubraapi/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import co.descubra.descubraapi.repository.EventService;
 
  
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EventController {
 	
 	public static final Logger log =  LoggerFactory.getLogger(AdministratorController.class);
